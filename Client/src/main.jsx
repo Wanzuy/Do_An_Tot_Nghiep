1fdd5262d4f9a5f3c1cf907ec9b12b89a1394e45
quangnd5203 <quangnd5203@gmail.com>
2025-04-23 02:48:42 +0000

    test

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "antd/dist/reset.css";
 import "./styles/app.scss";
-
 import "./i18n/i18n";
 
 createRoot(document.getElementById("root")).render(
